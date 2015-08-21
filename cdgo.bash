12f7025986b7b8cb9a09df53c3ad5d6de30897fc
Chris Broadfoot <cbro@google.com>
2015-08-20 21:20:06 -0700

    Add cdgo: changes directory to the source code of a Go package.

---
+++ cdgo.bash
@@ -0,0 +1,44 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function cdgo {
+  DIR=$(go list -e -f='{{.Dir}}' $1 2>/dev/null)
+  if [[ -n "$DIR" ]]; then
+    >&2 echo Changing into $DIR
+    cd $DIR
+  else
+    >&2 echo Could not find $1.
+  fi
+}
+
+function _gengolist {
+  go list ... > ~/.cdgo_pkglist
+}
+
+function _cdgo {
+  [[ -e ~/.cdgo_pkglist ]] || _gengolist
+
+  local cur
+  cur=${COMP_WORDS[COMP_CWORD]}
+
+
+  if [[ -n $cur ]]; then
+    n=$(echo $cur | tr '/' '\n' | wc -l | tr -d '[[:space:]]')
+    COMPREPLY=($(grep "^$cur" ~/.cdgo_pkglist | cut -d/ -f"1-$n" | uniq | sed 's|$|/|'))
+  else
+    COMPREPLY=($(cat ~/.cdgo_pkglist | cut -d/ -f1 | sort | uniq))
+  fi
+}
+
+complete -o nospace -F _cdgo cdgo
