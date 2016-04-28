da37e65fe07f7ba213d31ccbdbe76fac9ca2fb5f
Chris Broadfoot <cbro@golang.org>
2016-04-28 13:18:31 -0700

    rc/ps1: add printout of last unsuccessful exit code

--- rc/ps1.bash
+++ rc/ps1.bash
@@ -37,7 +37,20 @@ function pwd_depth_limit_2 {
   pwd | sed -e 's%/Users/cbro%~%' | sed -e "s|.*/\(.*/.*\)|\1|"
 }
 
+function _last_exit {
+  local exit=$?
+  local reset='\033[0m'
+  local red='\033[0;31m'
+  local green='\033[0;32m'
+  local symbol='\u220E'
+
+  if [ $exit != 0 ] && [ $exit != 130 ]; then
+    echo -e "${red}Exit code $exit$reset"
+  fi
+}
+
 # export for subshells
 export -f _parse_git_branch _parse_git_status _we_are_in_git_work_tree _pwd_depth_limit_2
 export PS1="\$(_pwd_depth_limit_2)\$(_parse_git_status)\$(_parse_git_branch) $ "
 export TERM=xterm-color
+export PROMPT_COMMAND=_last_exit
